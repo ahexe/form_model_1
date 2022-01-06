4eeb1b8c687dd319b1b838052e8f811af524ce6a
AmirHesam <hesamkangani@gmail.com>
2022-01-06 18:28:47 +0330

    add rating option

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>Your Rating</h2>
+  <select-button
+    v-model="selectValue"
+    :options="options"
+    @click="$emit('update:modelValue', selectValue)"
+  />
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      selectValue: this.modelValue,
+      options: ['Bad', 'Good', 'Perfect'],
+    };
+  },
+  watch: {
+    modelValue(value) {
+      this.selectValue = value;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+</style>
